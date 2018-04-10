27f6f3ff5cfb3ebf3292dd0de990c16fb9db3782
Tim Evoy <tievo17@gmail.com>
2018-04-09 18:23:48 -0700

    Update Deals and DealItem class visibility.

--- app/src/main/java/ca/timevoy/destinationcheers/Deals.java
+++ app/src/main/java/ca/timevoy/destinationcheers/Deals.java
@@ -6,29 +6,29 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Tim Evoy on 2018-04-04.
+ * This class is the blueprint for Deal items.
  */
 
-public class Deals {
+class Deals {
 
     /**
      * A deal item representing a happy hour deal.
      */
-    public class DealItem {
+    class DealItem {
 
-        public String day;
-        public String dealOfDay;
-        public String establishment;
-        public String location;
-        public String availability;
-        public String details;
-        public String website;
+        String day;
+        String dealOfDay;
+        String establishment;
+        String location;
+        String availability;
+        String details;
+        String website;
 
         /**
          * Deal item constructor to initialize properties.
          */
-        public DealItem(String day, String dealOfDay, String establishment,
-                        String location, String availability, String details, String website) {
+        DealItem(String day, String dealOfDay, String establishment,
+                 String location, String availability, String details, String website) {
 
             this.day = day;
             this.dealOfDay = dealOfDay;
@@ -40,45 +40,45 @@ public class Deals {
         }
 
         // GETTER for day
-        public String getDay() {
+        String getDay() {
             return day;
         }
 
         // GETTER for deal of the day
-        public String getDealOfDay() {
+        String getDealOfDay() {
             return dealOfDay;
         }
 
         // GETTER for establishment
-        public String getEstablishment() {
+        String getEstablishment() {
             return establishment;
         }
 
         // GETTER for location
-        public String getLocation() {
+        String getLocation() {
             return location;
         }
 
         // GETTER for availability
-        public String getAvailability() {
+        String getAvailability() {
             return availability;
         }
 
         // GETTER for details
-        public String getDetails() {
+        String getDetails() {
             return details;
         }
 
         // GETTER for website
-        public String getWebsite() { return website; }
+        String getWebsite() { return website; }
 
     }
 
     // An array of sample deal items.
-    public static final List<DealItem> ITEMS = new ArrayList<>();
+    static final List<DealItem> ITEMS = new ArrayList<>();
 
     // A map of sample deal items, by ID.
-    public static final Map<String, DealItem> ITEM_MAP = new HashMap<String, DealItem>();
+    static final Map<String, DealItem> ITEM_MAP = new HashMap<String, DealItem>();
 
     // Add deal item and its ID.
     private static void addDealItem(DealItem item) {
@@ -87,7 +87,7 @@ public class Deals {
     }
 
     // Create deal item's content to display.
-    public void createDealItems() {
+    void createDealItems() {
         addDealItem(new DealItem("Monday", "Mango Cheesecake & Mojitos", "Case Of The Mondays", "100 Warf Street", "3pm to 6pm", "Scrumptious Mango Cheescake that melts in your mouth $5/slice.\n\nRefreshingly delicious Mojitos (choice of Mint, Raspberry, or Blueberry) $5 for 2oz.", "www.caseofthemondays.com"));
         addDealItem(new DealItem("Tuesday", "Tuna Tataki & Gin + Tonics", "Tommy's Bistro", "200 Duglas Street", "3pm to 6pm", "Fresh from the Pacific ocean and locally caught Tuna Tataki $12 for half-size, $16 for full-size.\n\nTraditionally delicious and always fitting Gin + Tonics to enjoy $6 for 2oz.", "www.tommysbistro.com"));
         addDealItem(new DealItem("Wednesday", "Hot Wings & Whiskey Sours", "Just Wing'n It!", "300 Foul Bay Boulevard", "3pm to 6pm", "Tempt your tastebuds and tolerance levels with our house-made Hot Wings $6/pound.\n\nDo you feel the burn from the wings? Cool your tastebuds with a classic Whiskey Sour cocktail $5 for 1.5oz.", "www.justwingnit.com"));
